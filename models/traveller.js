d1c691dfa61863f5d254bda94c2b94825d97d1a3
Ross <rosso5792@gmail.com>
2020-01-08 16:46:52 +0000

    all pass except last 2, still need to complete

--- models/traveller.js
+++ models/traveller.js
@@ -3,19 +3,28 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+ const journeyStartLocation = this.journeys.map((locations) => { 
+   return locations.startLocation})
+ return journeyStartLocation;
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  const journeyEndLocation = this.journeys.map((locations) => { 
+    return locations.endLocation})
+  return journeyEndLocation;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  const journeyByTransport = this.journeys.filter((journey) => {
+    return journey.transport === transport});
+  return journeyByTransport;
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  const journeyByMinDistance = this.journeys.filter((length) => {
+    return length.distance > minDistance;
+})
+  return journeyByMinDistance;
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
